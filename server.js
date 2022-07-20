b448ee509648e5ab487190478e2f3b3de1efd348
rohitpatel80 <108566668+rohitpatel80@users.noreply.github.com>
2022-07-20 13:52:27 +0530

    Changed port to 3200

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3200;
 
 var app = express();
 
