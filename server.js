7789ff200e0ac74cbe55b89e19ba005c8e6c686d
rohitpatel80 <108566668+rohitpatel80@users.noreply.github.com>
2022-07-21 13:46:42 +0530

    3100

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.env.PORT || 3400;
+const port = process.env.PORT || 3100;
 
 var app = express();
 
