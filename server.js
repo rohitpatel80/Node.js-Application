d2ba3381e75f375d6d3aa12de56962e2fc7bb2a9
rohitpatel80 <108566668+rohitpatel80@users.noreply.github.com>
2022-07-22 10:54:45 +0530

    Commit to check webhook

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ hbs.registerHelper('screamIt', (text) => {
 app.get('/', (req,res) => {           //http request and response
 
 	res.render('home.hbs', {
-		pageTitle : 'New Home Page from jenkins with integration ',
+		pageTitle : 'New Home Page from jenkins with integration with webhook',
 		WelcomeMsg: 'Welcome to my website'
 	});
 
